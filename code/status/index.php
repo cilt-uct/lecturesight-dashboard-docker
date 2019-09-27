ac03f3257567a8348e11934a64673c2349f6cb07
Corne Oosthuizen <corne.oosthuizen@uct.ac.za>
2019-09-27 08:27:31 +0200

    Updates to status update/index.php

--- code/status/index.php
+++ code/status/index.php
@@ -4,61 +4,61 @@ $method = $_SERVER['REQUEST_METHOD'];
 
 if ($method == 'POST') {
 
-  $lsname = "lecturesight";
+    $lsname = "lecturesight";
 
-  if (isset($_POST['name'])) {
-	$lsname = $_POST['name'];
-  } else {
-	http_response_code(400);
-	die('No name field provided');
-  }
+    if (isset($_POST['name'])) {
+        $lsname = $_POST['name'];
+    } else {
+        http_response_code(400);
+        die('No name field provided');
+    }
 
-  if (!eregi("^([a-zA-Z0-9_-]){1,30}$", $lsname))
-  {
-	http_response_code(400);
-	die('Invalid name $lsname');
-  }
+    if (!preg_match("/^([a-zA-Z0-9_-]){1,30}$/", $lsname)) {
+        http_response_code(400);
+        die('Invalid name $lsname');
+    }
 
-  $uploaddir = "/code/venues/{$lsname}";
+    $uploaddir = "/code/venues/{$lsname}";
 
-  // Status
-  if(isset($_POST['status'])) {
-      $data = $_POST['status'];
-      $ret = file_put_contents("{$uploaddir}-status.txt", $data, LOCK_EX);
-      if($ret === false) {
-  	  http_response_code(500);
-          die('There was an error writing this file');
-      }
-  }
+    // Status
+    if(isset($_POST['status'])) {
+        $data = $_POST['status'];
+        $ret = file_put_contents("{$uploaddir}-status.txt", $data, LOCK_EX);
+        if($ret === false) {
+        http_response_code(500);
+            die('There was an error writing this file');
+        }
+    }
 
-  // Metrics
-  if(isset($_POST['metrics'])) {
-      $data = $_POST['metrics'];
-      $ret = file_put_contents("{$uploaddir}-metrics.json", $data, LOCK_EX);
-      if($ret === false) {
-  	  http_response_code(500);
-          die('There was an error writing this file');
-      }
-  }
+    // Metrics
+    if(isset($_POST['metrics'])) {
+        $data = $_POST['metrics'];
+        $ret = file_put_contents("{$uploaddir}-metrics.json", $data, LOCK_EX);
+        if($ret === false) {
+            http_response_code(500);
+            die('There was an error writing this file');
+        }
+    }
 
-  // Scene profile
-  if(isset($_POST['profile'])) {
-      $data = $_POST['profile'];
-      $ret = file_put_contents("{$uploaddir}-profile.scn", $data, LOCK_EX);
-      if($ret === false) {
-  	  http_response_code(500);
-          die('There was an error writing this file');
-      }
-  }
+    // Scene profile
+    if(isset($_POST['profile'])) {
+        $data = $_POST['profile'];
+        $ret = file_put_contents("{$uploaddir}-profile.scn", $data, LOCK_EX);
+        if($ret === false) {
+            http_response_code(500);
+            die('There was an error writing this file');
+        }
+    }
 
-  // Overview image
-  if (move_uploaded_file($_FILES['overview-image']['tmp_name'], $uploaddir . '-overview.png')) {
-    echo "File is valid, and was successfully uploaded.\n";
-  } else {
-     http_response_code(500);
-     echo "Upload failed";
-  }
+    // Overview image
+    if (move_uploaded_file($_FILES['overview-image']['tmp_name'], $uploaddir . '-overview.png')) {
+        echo "File is valid, and was successfully uploaded.\n";
+    } else {
+        http_response_code(500);
+        echo "Upload failed";
+    }
 } else {
+    date_default_timezone_set('Africa/Johannesburg');
     $dir   = '/code/venues';
     $files = glob('/code/venues/*-status.txt');
     $venue_list  = [];
